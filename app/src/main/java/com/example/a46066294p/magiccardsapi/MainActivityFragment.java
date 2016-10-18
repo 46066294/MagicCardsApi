ac05bc0a206829f1918d425a1d9a0a377f091bb5
46066294 <46066294p@iespoblenou.org>
2016-10-19 00:18:32 +0200

    Mostrar una listado de nombres cartas de ejemplo al arrancar

--- app/src/main/java/com/example/a46066294p/magiccardsapi/MainActivityFragment.java
+++ app/src/main/java/com/example/a46066294p/magiccardsapi/MainActivityFragment.java
@@ -5,11 +5,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +25,28 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ListView lvCards = (ListView)view.findViewById(R.id.lvCards);
+
+        String[] data = {
+                "Los 400 golpes",
+                "El odio",
+                "El padrino",
+                "El padrino. Parte II",
+                "Ocurrió cerca de su casa",
+                "Infiltrados",
+                "Umberto D."
+        };
+
+        items = new ArrayList<>(Arrays.asList(data));
+        adapter = new ArrayAdapter<>( //obs: extends BaseAdapter implements Filterable, ThemedSpinnerAdapter
+                getContext(),
+                R.layout.lv_cards_row,
+                R.id.tvCardName,
+                items
+        );
+        lvCards.setAdapter(adapter);
+
+        return view;
     }
 }
